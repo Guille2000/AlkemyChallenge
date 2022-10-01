8a7a8d63a548b19cee0bbd9a33c7d4baf190ce93
guille <guiacosta0000@gmail.com>
2022-10-01 13:58:25 -0300

    pipes

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -3,7 +3,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import Swal from 'sweetalert2'
-import { NgxSpinnerService } from 'ngx-spinner';;
 
 @Component({
   selector: 'app-login',
